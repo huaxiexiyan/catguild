89d881700004d93866c2952558472e71884975bf
xiyan <liuzhi0621@gmail.com>
2023-04-13 17:15:38 +0800

    spring security 简单测试

---
+++ catguild-ztest/src/main/java/cn/catguild/ztest/ZTestApp.java
@@ -0,0 +1,17 @@
+package cn.catguild.ztest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiyan
+ * @date 2023/3/18 12:25
+ */
+@SpringBootApplication
+public class ZTestApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZTestApp.class, args);
+	}
+
+}
---
+++ catguild-ztest/src/main/java/cn/catguild/ztest/controller/ResourceTestController.java
@@ -0,0 +1,22 @@
+package cn.catguild.ztest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * http://localhost:21000/resource/auth
+ *
+ * @author xiyan
+ * @date 2023/4/13 10:51
+ */
+@RequestMapping("/resource")
+@RestController
+public class ResourceTestController {
+
+	@GetMapping("/auth")
+	public String testAuth(){
+		return "访问成功";
+	}
+
+}
