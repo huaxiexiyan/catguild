1ee704b5df2bda0825bd7491e1a996bcea45e8db
xiyan <liuzhi0621@gmail.com>
2023-09-19 11:08:10 +0800

    feat: 添加新模块

---
+++ catguild-business/src/main/java/cn/catguild/business/BusinessApp.java
@@ -0,0 +1,11 @@
+package cn.catguild.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApp.class, args);
+    }
+}
