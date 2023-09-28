c7cb1654f0d046d4053ccd7c50ad1111c832aa0c
xiyan <liuzhi0621@gmail.com>
2023-09-28 14:53:17 +0800

    network: 部署修正

---
+++ catguild-network/gunicorn.conf.py
@@ -0,0 +1,19 @@
+import gevent.monkey
+
+gevent.monkey.patch_all()
+
+import multiprocessing
+
+loglevel = 'info'
+bind = "0.0.0.0:10000"
+# 配置了日志就需要目录存在，否则会启动失败
+# pidfile = "log/gunicorn.pid"
+# accesslog = "log/access.log"
+# errorlog = "log/debug.log"
+# True 就是后台启动
+# daemon = True
+
+# 启动的进程数
+workers = multiprocessing.cpu_count()
+worker_class = 'gevent'
+x_forwarded_for_header = 'X-FORWARDED-FOR'
