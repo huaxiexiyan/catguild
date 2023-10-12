d68a2f7074dfbd660d0fdbd6db5e1237fa92af10
xiyan <liuzhi0621@gmail.com>
2023-10-12 15:13:04 +0800

    sync

--- catguild-ztest/src/main/java/cn/catguild/ztest/ZTestApp.java
+++ catguild-ztest/src/main/java/cn/catguild/ztest/ZTestApp.java
@@ -2,11 +2,15 @@ package cn.catguild.ztest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author xiyan
  * @date 2023/3/18 12:25
  */
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class ZTestApp {
 
