07e9f1eddb3cd88aeae0a8c6415e3815ad7f67b5
xiyan <liuzhi0621@gmail.com>
2023-10-22 01:04:26 +0800

    添加erp的bo

---
+++ catguild-business/src/main/java/cn/catguild/business/erp/domain/type/ErpAttr.java
@@ -0,0 +1,22 @@
+package cn.catguild.business.erp.domain.type;
+
+import lombok.Data;
+
+/**
+ * @author xiyan
+ * @date 2023/10/19 11:46
+ */
+@Data
+public class ErpAttr {
+
+    /**
+     * 规格名
+     */
+    private String name;
+
+    /**
+     * 规格值
+     */
+    private String value;
+
+}
